670ad933a16216641c32d4e7d83e4a86e2bd2943
Sylwester Cebula <s.cebula99@gmail.com>
2022-06-14 20:09:05 +0200

    Add seeder for tables: roles, location

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
+using HotDeskAPI;
+using HotDeskAPI.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<HotDeskDbContext>();
+builder.Services.AddScoped<Seeder>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
+seeder.Seed();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Seeder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotDeskAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotDeskAPI
+{
+    public class Seeder
+    {
+        private readonly HotDeskDbContext _dbContext;
+        public Seeder(HotDeskDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+
+                if (!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+
+                if (!_dbContext.Locations.Any())
+                {
+                    var locations = GetLocations();
+                    _dbContext.Locations.AddRange(locations);
+                    _dbContext.SaveChanges();
+                }
+
+            }
+        }
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+                {
+                    new Role()
+                    {
+                        Name = "Admin"
+                    },
+
+                    new Role()
+                    {
+                        Name = "Employee"
+                    },
+                };
+
+            return roles;
+        }
+
+        private IEnumerable<Location> GetLocations()
+        {
+            var locations = new List<Location>()
+                {
+                    new Location()
+                    {
+                        Buiding = "A",
+                        Floor = 1,
+                        RoomNumber = 1
+                    },
+                    new Location()
+                    {
+                        Buiding = "A",
+                        Floor = 1,
+                        RoomNumber = 2
+                    },
+                    new Location()
+                    {
+                        Buiding = "A",
+                        Floor = 1,
+                        RoomNumber = 3
+                    },
+                    new Location()
+                    {
+                        Buiding = "B",
+                        Floor = 3,
+                        RoomNumber = 10
+                    },
+                    new Location()
+                    {
+                        Buiding = "B",
+                        Floor = 3,
+                        RoomNumber = 11
+                    },
+                    new Location()
+                    {
+                        Buiding = "B",
+                        Floor = 3,
+                        RoomNumber = 12
+                    },
+                };
+
+            return locations;
+        }
+
+    }
+}
